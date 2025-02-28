8358402396706c7e48ff06e11514b9515fbac86a
Aditya <code.aditya@gmail.com>
2025-02-28 20:40:43 +0530

    Update userscript to make it compatible with Violentmonkey

--- zerodha-kite-use-full-width.js
+++ zerodha-kite-use-full-width.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/codeaditya/browser-userscripts
 // @downloadURL  https://cdn.jsdelivr.net/gh/codeaditya/browser-userscripts@main/zerodha-kite-use-full-width.js
 // @updateURL    https://cdn.jsdelivr.net/gh/codeaditya/browser-userscripts@main/zerodha-kite-use-full-width.js
-// @version      2025.01.31
+// @version      2025.02.28
 // @description  Make Zerodha Kite use full width of the screen.
 // @author       Aditya <code.aditya at gmail>
 // @match        https://kite.zerodha.com/*
@@ -15,15 +15,7 @@
 
   GM_addStyle(`
     .app .wrapper {
-        max-width: initial;
-    }
-
-    .container {
-        max-width: initial;
-    }
-
-    .app .container .container-right {
-        max-width: initial;
+        max-width: initial !important;
     }
   `);
 })();
